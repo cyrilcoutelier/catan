9c5ece49d8e1b834524e0a7f2f4affb83f4aa52b
Cyril Coutelier <cyril@flat.io>
2025-07-14 14:53:56 +0200

    feat: add some init data

--- catan-model.ts/src/model/InitData.ts
+++ catan-model.ts/src/model/InitData.ts
@@ -1,6 +1,7 @@
 import type { DockType } from "./Dock";
 import type { TerrainTriggerValue, TerrainType } from "./Terrain";
 import type { Position } from "./Position";
+import DevelopmentCardType from "./DevelopmentCardType";
 
 export const CELL_DISTRIBUTION: Record<TerrainType, number> = {
 	desert: 1,
@@ -32,5 +33,17 @@ export const DOCK_DISTRIBUTION: Record<DockType, number> = {
 	ore: 1,
 	generic: 3,
 } as const;
+
+export const DEVELOPMENT_CARD_DISTRIBUTION: Record<DevelopmentCardType, number> = {
+	[DevelopmentCardType.RoadBuilding]: 2,
+	[DevelopmentCardType.YearOfPlenty]: 2,
+	[DevelopmentCardType.Monopoly]: 2,
+	[DevelopmentCardType.Knight]: 14,
+	[DevelopmentCardType.VictoryPoint]: 5,
+};
+
+export const START_NB_COLONIES = 5;
+export const START_NB_CITIES = 4;
+export const START_NB_ROADS = 15;
 
 export const DOCK_LOCATIONS: Position[] = [];
