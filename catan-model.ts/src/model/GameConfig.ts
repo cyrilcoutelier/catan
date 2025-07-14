827392bad4523c23837f4478af65d515a3ef1315
Cyril Coutelier <cyril@flat.io>
2025-07-14 14:53:21 +0200

    feat: add game configuration

---
+++ catan-model.ts/src/model/GameConfig.ts
@@ -0,0 +1,15 @@
+type GameConfig = {
+  randomColors: boolean;
+  randomPorts: boolean;
+  randomTiles: boolean;
+  fixedStartPositions: boolean;
+};
+
+export const BEGINNER_GAME_CONFIG: GameConfig = {
+  randomColors: false,
+  randomPorts: false,
+  randomTiles: false,
+  fixedStartPositions: true,
+}
+
+export type { GameConfig as default };
